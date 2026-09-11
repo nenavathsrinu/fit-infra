347c386b193532bd645449ab36a530e89e63c3b3
srinu <nenavath2013@gmail.com>
2026-09-12 01:05:36 +0530

    nat

--- modules/vpc.tf
+++ modules/vpc.tf
@@ -68,32 +68,8 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_eip" "nat" {
-  domain = "vpc"
-
-  tags = merge(local.tags, {
-    Name = "${var.network_name}-nat"
-  })
-}
-
-resource "aws_nat_gateway" "this" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id
-
-  depends_on = [aws_internet_gateway.this]
-
-  tags = merge(local.tags, {
-    Name = "${var.network_name}-nat"
-  })
-}
-
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this.id
-  }
 
   tags = merge(local.tags, {
     Name = "${var.network_name}-private"
